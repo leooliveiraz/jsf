5eca2e4e1b862f6edcf4d0147b2dc796762a6dff
Leonardo Rocha <leopdor@gmail.com>
2015-10-02 10:28:05 -0300

    Alterado classes.

--- Projeto01/src/main/java/controle/NomesBean.java
+++ Projeto01/src/main/java/controle/NomesBean.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlInputText;
 
 
 @ManagedBean
@@ -12,8 +14,18 @@ import javax.faces.bean.ViewScoped;
 public class NomesBean {
 	private String nome;
 	private List<String> nomes = new ArrayList<>();
+	
+	private HtmlInputText inputNome;
+	private HtmlCommandButton botaoAdicionar;
+	
 	public void adicionar() {
 		this.nomes.add(nome);
+		
+		if (this.nomes.size() > 3) {
+			this.inputNome.setDisabled(true);
+			this.botaoAdicionar.setDisabled(true);
+			this.botaoAdicionar.setValue("Muitos nomes adicionados...");
+		}
 	}
 	public String getNome() {
 		return nome;
@@ -24,4 +36,17 @@ public class NomesBean {
 	public List<String> getNomes() {
 		return nomes;
 	}
+	public HtmlInputText getInputNome() {
+		return inputNome;
+	}
+	public void setInputNome(HtmlInputText inputNome) {
+		this.inputNome = inputNome;
+	}
+	public HtmlCommandButton getBotaoAdicionar() {
+		return botaoAdicionar;
+	}
+	public void setBotaoAdicionar(HtmlCommandButton botaoAdicionar) {
+		this.botaoAdicionar = botaoAdicionar;
+	}
+			
 }
